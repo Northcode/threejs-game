1fd1daa3dfa0cefef792e4d46eb940479b99c039
Aadne E. Moldesæter <aadne.e.m@outlook.com>
2017-11-27 18:46:04 +0100

    Placed the jump logic to a more __logical__ position

--- player.js
+++ player.js
@@ -9,35 +9,33 @@ class Player extends GameObject
 	this.model.add(controls.getObject())
 	this.movespeed = 2
 	this.maxspeed = 5
-	this.jumppower = 8
+	this.jumppower = 4
 
     }
 
-    update(keyboard, scene) {
-	super.update(keyboard, scene)
+	update(keyboard, scene) {
+		super.update(keyboard, scene)
 
-	let rotMatrix = this.controls.getObject().matrix
-	let old_velocity = this.model.getLinearVelocity()
-	let current_speed = Math.sqrt(Math.pow(old_velocity.x, 2) + Math.pow(old_velocity.z,2))
-	let new_velocity = new THREE.Vector3(0,0,0)
+		let rotMatrix = this.controls.getObject().matrix
+		let old_velocity = this.model.getLinearVelocity()
+		let current_speed = Math.sqrt(Math.pow(old_velocity.x, 2) + Math.pow(old_velocity.z,2))
+		let new_velocity = new THREE.Vector3(0,0,0)
 
-	if (keyboard.pressed("W")) {
-	    new_velocity.z = -this.movespeed
-	}
-	if (keyboard.pressed("S")) {
-	    new_velocity.z = this.movespeed
-	}
-	if (keyboard.pressed("A")) {
-	    new_velocity.x = -this.movespeed
-	}
-	if (keyboard.pressed("D")) {
-	    new_velocity.x = this.movespeed
-	}
+		if (keyboard.pressed("W")) {
+			new_velocity.z = -this.movespeed
+		}
+		if (keyboard.pressed("S")) {
+			new_velocity.z = this.movespeed
+		}
+		if (keyboard.pressed("A")) {
+			new_velocity.x = -this.movespeed
+		}
+		if (keyboard.pressed("D")) {
+			new_velocity.x = this.movespeed
+		}
 
-	if (current_speed < this.maxspeed) {
-	    new_velocity.applyMatrix4(rotMatrix)
-	    new_velocity.y = 0
-
+		new_velocity.applyMatrix4(rotMatrix)
+		new_velocity.y = 0
 
 		if (keyboard.pressed("space")) {
 
@@ -50,13 +48,13 @@ class Player extends GameObject
 				new_velocity.y = this.jumppower
 			}
 		}
+		
+		if (current_speed < this.maxspeed) {
+			let tot_speed = new THREE.Vector3()
+			tot_speed.addVectors(old_velocity, new_velocity)
 
-	    let tot_speed = new THREE.Vector3()
-	    tot_speed.addVectors(old_velocity, new_velocity)
-
-
-	    this.model.setLinearVelocity(tot_speed)
-	    this.model.setAngularFactor(new THREE.Vector3(0,0,0))
+			this.model.setLinearVelocity(tot_speed)
+			this.model.setAngularFactor(new THREE.Vector3(0,0,0))
+		}
 	}
-    }
 }
