8c6534df9f596003694388dc40caed00738f18f7
Aadne E. Moldesæter <aadne.e.m@outlook.com>
2017-11-27 18:33:05 +0100

    JUMPING!

--- player.js
+++ player.js
@@ -4,20 +4,21 @@ class Player extends GameObject
 	super()
 	model.setAngularFactor(new THREE.Vector3(0,0,0))
 	this.model = model
+	this.model.castShadow = true
 	this.controls = controls
 	this.model.add(controls.getObject())
-	this.movespeed = 0.5
+	this.movespeed = 2
 	this.maxspeed = 5
+	this.jumppower = 8
 
     }
-    
+
     update(keyboard, scene) {
 	super.update(keyboard, scene)
-	
+
 	let rotMatrix = this.controls.getObject().matrix
 	let old_velocity = this.model.getLinearVelocity()
 	let current_speed = Math.sqrt(Math.pow(old_velocity.x, 2) + Math.pow(old_velocity.z,2))
-
 	let new_velocity = new THREE.Vector3(0,0,0)
 
 	if (keyboard.pressed("W")) {
@@ -36,6 +37,20 @@ class Player extends GameObject
 	if (current_speed < this.maxspeed) {
 	    new_velocity.applyMatrix4(rotMatrix)
 	    new_velocity.y = 0
+
+
+		if (keyboard.pressed("space")) {
+
+			let raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 2 )
+			raycaster.ray.origin.copy( this.model.position )
+			let intersections = raycaster.intersectObjects( scene.children )
+
+			if (intersections.length > 0) {
+				console.log("Jump!");
+				new_velocity.y = this.jumppower
+			}
+		}
+
 	    let tot_speed = new THREE.Vector3()
 	    tot_speed.addVectors(old_velocity, new_velocity)
 
