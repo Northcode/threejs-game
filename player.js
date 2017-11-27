8a50797bba279c7b11052b526003fd700c5f3f1c
Andreas Larsen <northcode.no@gmail.com>
2017-11-27 19:58:18 +0100

    Better movement

--- player.js
+++ player.js
@@ -7,54 +7,64 @@ class Player extends GameObject
 	this.model.castShadow = true
 	this.controls = controls
 	this.model.add(controls.getObject())
-	this.movespeed = 2
-	this.maxspeed = 5
-	this.jumppower = 4
+	this.movespeed = 5
+	this.maxspeed = 10
+	this.jumppower = 30
 
     }
 
-	update(keyboard, scene) {
-		super.update(keyboard, scene)
+    update(keyboard, scene) {
+	super.update(keyboard, scene)
 
-		let rotMatrix = this.controls.getObject().matrix
-		let old_velocity = this.model.getLinearVelocity()
-		let current_speed = Math.sqrt(Math.pow(old_velocity.x, 2) + Math.pow(old_velocity.z,2))
-		let new_velocity = new THREE.Vector3(0,0,0)
+	let rotMatrix = this.controls.getObject().matrix
+	let old_velocity = this.model.getLinearVelocity()
+	let current_speed = Math.sqrt(Math.pow(old_velocity.x, 2) + Math.pow(old_velocity.z,2))
+	let new_velocity = new THREE.Vector3(0,0,0)
 
-		if (keyboard.pressed("W")) {
-			new_velocity.z = -this.movespeed
-		}
-		if (keyboard.pressed("S")) {
-			new_velocity.z = this.movespeed
-		}
-		if (keyboard.pressed("A")) {
-			new_velocity.x = -this.movespeed
-		}
-		if (keyboard.pressed("D")) {
-			new_velocity.x = this.movespeed
-		}
-
-		new_velocity.applyMatrix4(rotMatrix)
-		new_velocity.y = 0
-
-		if (keyboard.pressed("space")) {
-
-			let raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 2 )
-			raycaster.ray.origin.copy( this.model.position )
-			let intersections = raycaster.intersectObjects( scene.children )
-
-			if (intersections.length > 0) {
-				console.log("Jump!");
-				new_velocity.y = this.jumppower
-			}
-		}
-		
-		if (current_speed < this.maxspeed) {
-			let tot_speed = new THREE.Vector3()
-			tot_speed.addVectors(old_velocity, new_velocity)
-
-			this.model.setLinearVelocity(tot_speed)
-			this.model.setAngularFactor(new THREE.Vector3(0,0,0))
-		}
+	if (keyboard.pressed("W")) {
+	    new_velocity.z = -this.movespeed
 	}
+	if (keyboard.pressed("S")) {
+	    new_velocity.z = this.movespeed
+	}
+	if (keyboard.pressed("A")) {
+	    new_velocity.x = -this.movespeed
+	}
+	if (keyboard.pressed("D")) {
+	    new_velocity.x = this.movespeed
+	}
+
+	new_velocity.applyMatrix4(rotMatrix)
+	new_velocity.y = old_velocity.y
+
+	if (keyboard.pressed("space")) {
+
+	    let raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 2 )
+	    raycaster.ray.origin.copy( this.model.position )
+	    let intersections = raycaster.intersectObjects( scene.children )
+
+	    if (intersections.length > 0) {
+		console.log("Jump!");
+		new_velocity.y = this.jumppower
+	    }
+	}
+
+	if (current_speed < this.maxspeed) {
+
+	    // if ((new_velocity.x > 0 && old_velocity.x < 0)
+	    // 	|| (new_velocity.x < 0 && old_velocity.x > 0)) {
+	    // 	old_velocity.x = 0
+	    // }
+	    // if ((new_velocity.z > 0 && old_velocity.z < 0)
+	    // 	|| (new_velocity.z > 0 && old_velocity.z > 0)) {
+	    // 	old_velocity.z = 0
+	    // }
+
+	    let tot_speed = new THREE.Vector3()
+	    // tot_speed.addVectors(old_velocity, new_velocity)
+
+	    this.model.setLinearVelocity(new_velocity)
+	}
+	this.model.setAngularFactor(new THREE.Vector3(0,0,0))
+    }
 }
